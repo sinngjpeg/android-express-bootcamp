6039a898e349e5bd6e6291de10baf3b98b187eb9
unknown <Ingrid Silva>
2022-02-09 00:49:54 -0300

    implementa conexao da interface grafica e logica

--- Layout/app/src/main/java/com/sinngjpeg/layout/MainActivity.java
+++ Layout/app/src/main/java/com/sinngjpeg/layout/MainActivity.java
@@ -3,6 +3,7 @@ package com.sinngjpeg.layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtTitle = findViewById(R.id.title);
+        TextView txtSubTitle = findViewById(R.id.subtitle);
+
+        String title = "finge que veio do banco de dados";
+
+        txtTitle.setText("Olá Mundo!");
+        txtSubTitle.setText(title);
     }
 }
