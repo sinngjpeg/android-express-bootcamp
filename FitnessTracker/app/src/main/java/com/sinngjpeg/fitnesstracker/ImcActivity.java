829956421dfce11328c6996851d8ef93dae237f9
unknown <Ingrid Silva>
2022-02-23 00:11:26 -0300

    implements method to calculate IMC

--- FitnessTracker/app/src/main/java/com/sinngjpeg/fitnesstracker/ImcActivity.java
+++ FitnessTracker/app/src/main/java/com/sinngjpeg/fitnesstracker/ImcActivity.java
@@ -1,14 +1,80 @@
 package com.sinngjpeg.fitnesstracker;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ImcActivity extends AppCompatActivity {
+
+    private EditText editHeight;
+    private EditText editWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imc);
+
+        editHeight = findViewById(R.id.edt_imc_height);
+        editWeight = findViewById(R.id.edt_imc_weight);
+
+        Button btnSend = findViewById(R.id.btn_imc_send);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!validate()) {
+                    Toast.makeText(ImcActivity.this, R.string.fields_message, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                String sHeight = editHeight.getText().toString();
+                String sWeight = editWeight.getText().toString();
+
+                int height = Integer.parseInt(sHeight);
+                int weight = Integer.parseInt(sWeight);
+                double result = calculateIMC(height, weight);
+                Log.d("Teste", "resultado: " + result);
+
+                int imcResponseId = imcResponse(result);
+                Toast.makeText(ImcActivity.this, imcResponseId, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @StringRes
+    private int imcResponse(double imc) {
+        if (imc < 15)
+            return R.string.imc_severely_low_weight;
+        else if (imc < 16)
+            return R.string.imc_very_low_weight;
+        else if (imc < 18.5)
+            return R.string.imc_low_weight;
+        else if (imc < 25)
+            return R.string.normal;
+        else if (imc < 30)
+            return R.string.imc_high_weight;
+        else if (imc < 35)
+            return R.string.imc_so_high_weight;
+        else if (imc < 40)
+            return R.string.imc_severely_high_weight;
+        else
+            return R.string.imc_extreme_weight;
+
+    }
+
+    private double calculateIMC(int height, int weight) {
+        return weight / (((double) height / 100) * ((double) height / 100));
+    }
+
+    private boolean validate() {
+        return (!editHeight.getText().toString().startsWith("0")
+                && !editWeight.getText().toString().startsWith("0")
+                && !editHeight.getText().toString().isEmpty()
+                && !editWeight.getText().toString().isEmpty());
     }
 }
