cb163190e5bbcb50025edfe89f867da80df63b9c
unknown <Ingrid Silva>
2022-02-23 01:24:58 -0300

    implements base recycleview and adapter

--- FitnessTracker/app/src/main/java/com/sinngjpeg/fitnesstracker/MainActivity.java
+++ FitnessTracker/app/src/main/java/com/sinngjpeg/fitnesstracker/MainActivity.java
@@ -1,24 +1,65 @@
 package com.sinngjpeg.fitnesstracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View btnImc;
+    //    private View btnImc;
+    private RecyclerView rvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnImc = findViewById(R.id.btn_imc);
-        btnImc.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-            startActivity(intent);
-        });
+        rvMain = findViewById(R.id.rv_main);
+
+       /* -> definir comportamento de exibição do layout da recylceview
+                pode ser:  mosaico, grid , linear ( horizontal | vertical )
+        */
+
+        rvMain.setLayoutManager(new LinearLayoutManager(this));
+        MainAdapter adapter = new MainAdapter();
+        rvMain.setAdapter(adapter);
+//        btnImc = findViewById(R.id.btn_imc);
+//        btnImc.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+//            startActivity(intent);
+//        });
+    }
+
+    private class MainAdapter extends RecyclerView.Adapter {
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 15;
+        }
+    }
+
+    // View da Celula que está dentro do recycle view
+    private class MainViewHolder extends RecyclerView.ViewHolder {
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
